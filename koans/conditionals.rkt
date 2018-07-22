898dfc29d6226058343d41266098fdde2758f295
Sage Gerard <zyrolasting@gmail.com>
2018-07-22 12:57:32 -0400

    Prefer backticks when mentioning forms inline

--- koans/conditionals.rkt
+++ koans/conditionals.rkt
@@ -18,8 +18,8 @@
 ; If you've seen things like `(a && b)` or `(a || b)`, then that's what
 ; you are dealing with here.
 ;
-;   (and) returns #t if all following tests are #t. It stops at the first #f.
-;   (or) returns #t if at least one following test is #t. It stops at the first #t.
+;   - `and` returns #t if all following tests are #t. It stops at the first #f.
+;   - `or` returns #t if at least one following test is #t. It stops at the first #t.
 (check-equal? (and #t #t) "?")
 (check-equal? (or #f #t) "?")
 (check-equal? (and #f #t #t) "?")
@@ -33,13 +33,13 @@
 (check-equal? (not #t) "?")
 (check-equal? (not "nah") "?")
 
-; The (cond) form evaluates tests in sequence, and will only evaluate the
-; expressions that follow the first test that results in #t. The (else)
-; clause can be used in a (cond) to handle a default case.
+; The `cond` form evaluates tests in sequence, and will only evaluate the
+; expressions that follow the first test that results in #t. The `else`
+; clause can be used in a `cond` to handle a default case.
 ;
 ; Remember that you can switch bracket styles to help readers not get lost
 ; in an ocean of parentheses. Square brackets are used in the Racket Guide
-; to more strongly emphasize which expressions provided directly to (cond).
+; to more strongly emphasize which expressions provided directly to `cond`.
 (define key "a")
 (check-equal?
   (cond
